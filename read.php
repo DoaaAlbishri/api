4aa577cb0b1df5fd2b9391d616905b81593a7e79
Doaa Albishri <90598036+DoaaAlbishri@users.noreply.github.com>
2023-09-27 00:11:30 +0300

    Create read.php

---
+++ read.php
@@ -0,0 +1,38 @@
+[
+    {
+        "id": "1",
+        "username": "vendetta",
+        "firstname": "Mushe",
+        "lastname": "Abdul-Hakim",
+        "email": "musheabdulhakim99@gmail.com",
+        "phone": "0542441933",
+        "avatar": "\/opt\/lampp\/temp\/phpBI156N",
+        "password": "$2y$10$w\/Mlff4D8r8na63sum221eTIsQmwLMUdDivFnZ5lbSQwHEkibhZJ.",
+        "created_at": "2021-02-13 22:00:56",
+        "updated_at": "2021-02-13 22:00:56"
+    },
+    {
+        "id": "2",
+        "username": "bfoster",
+        "firstname": "Walk-In",
+        "lastname": "Customer",
+        "email": "info@bfoster.com",
+        "phone": "0342020369",
+        "avatar": "1613288323_dbc66a90b1c291bffaa4.jpg",
+        "password": "$2y$10$zy8n93A1cr5p3I8P8AxgGuqtlQzTEsjnP8sIT6RUzZWmcXwP\/t1qy",
+        "created_at": "2021-02-14 07:38:43",
+        "updated_at": "2021-02-14 07:38:43"
+    },
+    {
+        "id": "3",
+        "username": "doaa",
+        "firstname": "Doaa Doaa",
+        "lastname": "Ali Ali",
+        "email": "doaa243648@gmail.com",
+        "phone": "96653665788",
+        "avatar": "1689338053_07a804a85990b31fcd82.png",
+        "password": "$2y$10$jJaSkzr02XJ0yUDT4Zys\/OI8Ai2Z8lILVsMr2ot6p5B.udXIoitiS",
+        "created_at": "2023-07-14 15:34:13",
+        "updated_at": "2023-07-14 15:34:13"
+    }
+]
