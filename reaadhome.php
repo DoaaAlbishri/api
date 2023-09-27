80a42cfe910cc7ab56ab0df1b532176fbf2faeb8
Doaa Albishri <90598036+DoaaAlbishri@users.noreply.github.com>
2023-09-27 14:15:47 +0300

    Update reaadhome.php

--- reaadhome.php
+++ reaadhome.php
@@ -1,5 +1,3 @@
 [
 {"id":"1","title":"برنامج تعزيز ولاء الموظفين","description":"يهدف برنامج ولاء الى برنامج تعزيز ولاء الموظفين"}
-,
-{"id":"2","title":"Employee loyalty program","description":"The loyalty program aims to enhance employee loyalty"}
 ]
