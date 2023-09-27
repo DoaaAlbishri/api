944547f35c0e21721601e12b552856a692f6f993
Doaa Albishri <90598036+DoaaAlbishri@users.noreply.github.com>
2023-09-27 19:03:54 +0300

    Return readevents.php

--- readevents.php
+++ readevents.php
@@ -1,1 +1,2 @@
+
 [{"id":"2","title":"بطولة بلاستشين","description":"بطولة تهدف الى .....","group_name":"event"},{"id":"3","title":"فعالية اخرى","description":"الهدف من الفعالية","group_name":"event"}]
