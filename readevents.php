3132c89f2185ebd7efde27689d5f9ad3df2b01ff
Doaa Albishri <90598036+DoaaAlbishri@users.noreply.github.com>
2023-09-28 10:36:28 +0300

    Update readevents.php

--- readevents.php
+++ readevents.php
@@ -1,4 +1,30 @@
 
 [{"id":"2","title":"بطولة بلاستشين","description":"هدف بطولة بلاستيشن للموظفين هو تعزيز الروح الرياضية بين الموظفين وتحفيزهم على العمل بجهد أكبر من خلال التنافس الإيجابي. وبمشاركة الموظفين في البطولة، ستزيد الانتماء للشركة وتعزيز العلاقات الاجتماعية بينهم، بالإضافة إلى رفع الروح المعنوية للموظفين وتحقيق مستوى عالي من الإنتاجية.
 ","group_name":"event"},{"id":"3","title":"فعالية اخرى","description":" الهدف العام الذي يراد تحقيقه من الفعالية ويشكل التوجهات الأساسية لتصميم وتخطيط الفعالية بالكامل. وتعد كلمة الهدف في الفعالية أمرًا حيويًا للغاية حيث يجب أن يكون الهدف جليًا وواضحًا لضمان أن يتم استثمار الجهود والموارد بصورة فعالة ومثمرة. وبمعرفة الهدف بوضوح، يصبح بالإمكان تصميم الفعالية بالطريقة الأكثر فاعلية والتي تلبي المتطلبات المحددة مسبقًا وتحقق أكبر قدر من النجاح والإرضاء للجميع. وهذا يؤدي في نهاية المطاف إلى تحقيق نتائج إيجابية وملموسة، سواء كان الهدف هو تحفيز فريق العمل أو زيادة المبيعات أو تعزيز العلاقات العامة أو أي هدف آخر.
-","group_name":"event"}]
+","group_name":"event"},
+{"id":"4","title":"بطولة بلاستشين","description""<div class="com-content-article item-page" itemscope="" itemtype="https://schema.org/Article">
+    <meta itemprop="inLanguage" content="ar-AA">
+    
+    
+        <div class="page-header">
+        <h1 itemprop="headline">
+            بطولة بلايستيشن لمنسوبي برنامج مستشفى قوى الامن بمكة        </h1>
+                            </div>
+                
+    <div class="icons">
+        <div class="float-end">
+            <div>
+                <a href="/ar/walaa-events/440-btwlt-blaystyshn-lmnswby-brnamj-msshtfy-qwy-alamn-bmkt?task=article.edit&amp;a_id=440&amp;return=aHR0cHM6Ly9pbnRyYW5ldC5zZmhtLm1lZC5zYS9hci93YWxhYS1ldmVudHMvNDQwLWJ0d2x0LWJsYXlzdHlzaG4tbG1uc3dieS1icm5hbWotbXNzaHRmeS1xd3ktYWxhbW4tYm1rdA==" aria-describedby="editarticle-440"><span class="icon-edit" aria-hidden="true"></span>
+    تعديل<div role="tooltip" id="editarticle-440">
+    Published Article</div>
+</a>            </div>
+        </div>
+    </div>
+                                                <div itemprop="articleBody" class="com-content-article__body">
+        <p style="text-align: center;"><span style="font-family: webdings; color: #239e91;"><strong><span style="font-size: 18px;"><span style="font-size: 36px;">بطولة بلايستيشن لمنسوبي برنامج مستشفى قوى الامن بمكة</span><br></span></strong></span></p>
+<p style="text-align: center;"><span style="font-family: webdings; color: #239e91;"><strong><span style="font-size: 18px;"><img src="/images/btwlt%20alblaystyshn%20fyfa%202023.png" width="817" height="1453" loading="lazy" data-path="local-images:/btwlt alblaystyshn fyfa 2023.png"><br><br><br></span></strong></span></p>
+<p style="text-align: center;"><a class="yt-clearfix buttons-style-glass ytyt_btn ul_65152c789df9b7366861261695886456 yt_btn-none yt_btn-lg " href="/ar/walaa-registration-form" target="_self" data-placement="" title=""><span class="span-buttons"><span> التسجيل </span> </span></a>     </p></div>
+
+        
+                                        </div>"}
+]
